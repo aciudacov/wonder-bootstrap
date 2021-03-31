1c28df9ec84ab337b482c58b637d62caccf8f7c8
Andrei Ciudacov <andrei.ciudacov@extendaretail.com>
2021-04-01 01:08:47 +0300

    added quiz files

--- scripts/quiz_generator.js
+++ scripts/quiz_generator.js
@@ -70,14 +70,29 @@ var products = [
     }
 ];
 
+function loadQuizBody(id){
+    var quizClass = "#quiz" + id + "_body";
+    $(quizClass).load("./quiz/" + id + ".html");
+}
+
 function generateQuizes(){
     var num = 1;
     products.forEach(element => {
-        toAppendString = "<div class=\"col\"><div class=\"card shadow-sm\"><img src=\"img/quiz/quiz" + num + ".jpg\" class='bg-placeholder-img card-img-top' width=\"100%\" height=\"225\""
-          + "role=\"img\" aria-label=\"Quiz thumbnail\" preserveAspectRatio=\"xMidYMid slice\" focusable=\"false\"><div class=\"card-body\"><p class=\"card-text\">" + element.text
-          + "</p><div class=\"d-flex justify-content-between align-items-center\"><div class=\"btn-group\"><button type=\"button\" class=\"btn btn-sm btn-outline-secondary\">Пройти тест</button>"
-          + "</div></div></div></div></div>";
+        toAppendString = "<div class=\"col\" id=\"quiz" + num + "\"><div class=\"card shadow-sm\"><img src=\"img/quiz/quiz"
+        + num + ".jpg\" class='bg-placeholder-img card-img-top' width=\"100%\" height=\"225\""
+        + "role=\"img\" aria-label=\"Quiz thumbnail\" preserveAspectRatio=\"xMidYMid slice\" focusable=\"false\">"
+        + "<div class=\"card-body\"><p class=\"card-text\">" + element.text
+        + "</p><div class=\"d-flex justify-content-between align-items-center\"><div class=\"btn-group\">"
+        + "<button type=\"button\" class=\"btn btn-sm btn-outline-secondary\" data-bs-toggle=\"modal\" data-bs-target=\"#quiz_modal" + num
+        + "\">Пройти тест</button></div></div></div></div></div>"
+        + "<div class=\"modal fade\" id=\"quiz_modal" + num + "\" tabindex=\"-1\" aria-labelledby=\"conceptModalLabel\" style=\"display: none;\" aria-hidden=\"true\">"
+        + "<div class=\"modal-dialog modal-xl modal-dialog-scrollable\"><div class=\"modal-content\">"
+        + "<div class=\"modal-header\"><h5 class=\"modal-title\" id=\"quiz" + num + "ModalLabel\">" + element.text + "</h5>"
+        + "<button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"modal\" aria-label=\"Close\"></button></div>"
+        + "<div class=\"modal-body\" id=\"quiz" + num + "_body\"> +</div><div class=\"modal-footer\"></div></div></div></div>";
         $("#quiz_container").append(toAppendString);
+        loadQuizBody(num);
         num++;
       });
+
 }
