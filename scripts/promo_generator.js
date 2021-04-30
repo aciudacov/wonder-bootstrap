cbc74a3b9ca7887ede7d457bbbc81fe675b18391
Andrei Ciudacov <andrei.ciudacov@extendaretail.com>
2021-04-30 22:04:51 +0300

    add transition from promo to catalog

--- scripts/promo_generator.js
+++ scripts/promo_generator.js
@@ -2,27 +2,32 @@ var promo_items = [
 	{
 		"name":"Липосомальный куркумин",
 		"image":"https://incoral.com/src/new/product/catalog/2165-liposomalnyy-kurkumin-100-ml.jpg",
-		"offer":"-10%"
+		"offer":"-10%",
+		"prod_id":"2165"
 	},
 	{
 		"name":"Гриффония (60 капсул)",
 		"image":"https://incoral.com/src/new/product/catalog/91848-griffoniya-60-kapsul.jpg",
-		"offer":"-10%"
+		"offer":"-10%",
+		"prod_id":"91848"
 	},
 	{
 		"name":"Абрикотаб (60 таблеток)",
 		"image":"https://incoral.com/src/new/product/catalog/2134-abrikotab-60-tabletok.jpg",
-		"offer":"4+1"
+		"offer":"4+1",
+		"prod_id":"2134"
 	},
 	{
 		"name":"Хлорелла (180 таблеток)",
 		"image":"https://incoral.com/src/new/product/catalog/2197-hlorella-180-tabletok.jpg",
-		"offer":"-10%"
+		"offer":"-10%",
+		"prod_id":"2129"
 	},
 	{
 		"name":"Би-курунга (90 таблеток)",
 		"image":"https://incoral.com/src/new/product/catalog/757-bi-kurunga-90-tabletok.jpg",
-		"offer":"3+1"
+		"offer":"3+1",
+		"prod_id":"2154"
 	}
 ]
 function generatePromos(){
@@ -33,7 +38,7 @@ function generatePromos(){
         + "role=\"img\" aria-label=\"Quiz thumbnail\" preserveAspectRatio=\"xMidYMid slice\" focusable=\"false\">"
         + "<div class=\"card-body\"><p class=\"card-text\">" + element.name
         + "</p><div class=\"d-flex justify-content-between align-items-center\"><div class=\"btn-group\">"
-        + "<button type=\"button\" class=\"btn btn-lg btn-outline-primary\">"+ element.offer +"</button></div></div></div></div></div>";
+        + "<button type=\"button\" class=\"btn btn-lg btn-outline-primary\" data-bs-target=\"#catalog_modal\" data-bs-toggle=\"modal\" data-bs-dismiss=\"modal\" onclick=\"focusElement(" + element.prod_id + ")\">"+ element.offer +"</button></div></div></div></div></div>";
         $("#promo_container").append(toAppendString);
         num++;
       });
